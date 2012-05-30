322ec67e5b48cc449ed22823e554c134bbd1d046
Carl Boettiger <cboettig@gmail.com>
2012-05-30 13:07:50 -0700

    Made citet and citep share subroutines, closes #5

--- R/citep.R
+++ R/citep.R
@@ -21,29 +21,45 @@
 #' citep("Halpern2006")
 #' 
 citep <- function(x){
+  out <- cite(x, inline_format=authoryear_p)
+  I(paste("(", paste(out, collapse="; "), ")", sep=""))
+}
 
+
+#' Add a textual citation 
+#'
+#' @param x a doi or list of dois, or a bibentry (or list of bibentries)
+#' @return a text inline citation
+#' @details Stores the full citation in a "works_cited" list,
+#' which can be printed with \code{\link{bibliography}}
+#' @export
+#' @import knitr
+#' @examples
+#' library(knitcitations)
+#'  citet("10.3998/3336451.0009.101")
+#'  ## Read in the bibtex information for some packages:
+#'  knitr <- citation("knitr") 
+#'  citet(knitr)
+#'  # generate the full bibliography:
+#'  bibliography()
+#' ## Assign a citation key to a doi and then use it later:
+#' citet(c(Halpern2006="10.1111/j.1461-0248.2005.00827.x"))
+#' citet("Halpern2006")
+#' 
+citet <- function(x){
+  out <- cite(x, inline_format=authoryear_t)
+  I(paste(paste(out, collapse="; "), sep=""))
+}
+
+
+
+
+cite <- function(x, inline_format){
   ## initialize the works cited list if not avaialble
   if(is.null(getOption("works_cited")))
     cleanbib()
-
-  ## HANDLE NAMED & UNAMED DOIs
-#  if(is(x, "character")){
-#    named_dois <- getOption("named_dois")
-#    if(!is.null(names(x))){ # if x is named, add it to the keylist
-#      # avoid repeat entries?
-#      options(named_dois = c(named_dois, x) )
-#    } else {
-#      # See if x is a bibkey name for the doi, rather than the doi
-#      y <- named_dois[match(x, names(named_dois))]
-#      if(!is.na(y)) # match found, swap doi for the key
-#        x <- y
-#    }
-#  }
-
-
-  current <- bibliography()
+ current <- bibliography()
   existing <- sapply(current, function(x) x$key)
-
   out <- sapply(x,function(x){
     m <- match(x, existing)
     if(!is.na(m))
@@ -58,17 +74,15 @@ citep <- function(x){
       entry <- create_bibkey(entry, key=key)
     }
 
-
     if(!is(entry, "bibentry")) # if it's not a bibentry, print "?"
       out <- I("?")
     else {
       ## keep track of what we've cited so far
       options(works_cited = c(getOption("works_cited"), entry))
-      out <-  authoryear_p(entry)
+      out <-  inline_format(entry)
     }
     out
   })
-  I(paste("(", paste(out, collapse="; "), ")", sep=""))
 }
 
 
