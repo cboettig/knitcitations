84aac2ab16d376097b793848a7bb556477162941
Carl Boettiger <cboettig@gmail.com>
2014-05-30 16:52:36 -0700

    update cite_options to control more options

--- R/cite_options.R
+++ R/cite_options.R
@@ -7,12 +7,19 @@
 #' @param bibtex_data whether we should write to a bibtex file instead of 
 #'  the knitcitationsCache environment for tracking bibliographic 
 #'  information of what we have already cited.  
+#' @param citation_format pandoc-style citations, compatibility (with version 0.5 or earlier), or text (show citations as plain text without hyperlinks)
+#' @param ... additional arguments to BibOptions
 #' @return updates the option specified for the duration of the session. 
 #' @export 
-cite_options <- function(tooltip = FALSE, linked=TRUE, numerical=FALSE, bibtex_data = FALSE){
+cite_options <- function(tooltip = FALSE, linked=TRUE, numerical=FALSE, bibtex_data = FALSE, citation_format = "compatibility", ...){
   assign("tooltip",  tooltip, envir=knitcitations_options)
   assign("linked", linked,  envir=knitcitations_options)
   assign("numerical", numerical,  envir=knitcitations_options)
   assign("bibtex_data", bibtex_data, envir=knitcitations_options)
+  options(citation_format = citation_format)
+  if(numerical)
+    BibOptions(cite.style="numeric")
+  BibOptions(...)
+
 }
 
