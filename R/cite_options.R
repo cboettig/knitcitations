acf7798bd75e53d612fb06bf49aedb375b4e659e
Carl Boettiger <cboettig@gmail.com>
2014-05-30 19:47:31 -0700

    update cite_options

--- R/cite_options.R
+++ R/cite_options.R
@@ -3,7 +3,6 @@
 #' @param tooltip Provide a javascript tooltip over the in-line citation? 
 #'  (requires twitter-bootstrap tooltip JS, see details).  
 #' @param linked Add an HTML link around the in-line citation? 
-#' @param numerical Use numerical instead of author-year format citations? 
 #' @param bibtex_data whether we should write to a bibtex file instead of 
 #'  the knitcitationsCache environment for tracking bibliographic 
 #'  information of what we have already cited.  
@@ -11,15 +10,11 @@
 #' @param ... additional arguments to BibOptions
 #' @return updates the option specified for the duration of the session. 
 #' @export 
-cite_options <- function(tooltip = FALSE, linked=TRUE, numerical=FALSE, bibtex_data = FALSE, citation_format = "compatibility", ...){
+cite_options <- function(tooltip = FALSE, linked=TRUE, bibtex_data = FALSE, citation_format = "compatibility", ...){
   assign("tooltip",  tooltip, envir=knitcitations_options)
   assign("linked", linked,  envir=knitcitations_options)
-  assign("numerical", numerical,  envir=knitcitations_options)
   assign("bibtex_data", bibtex_data, envir=knitcitations_options)
   options(citation_format = citation_format)
-  if(numerical)
-    BibOptions(cite.style="numeric")
-  BibOptions(...)
-
+  invisible(BibOptions(...))
 }
 
