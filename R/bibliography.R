5720c9bade9c9b35f7f7aa191de4d73520a17356
Carl Boettiger <cboettig@gmail.com>
2014-06-13 00:52:34 -0400

    trial version of csl formatting implemented #38

--- R/bibliography.R
+++ R/bibliography.R
@@ -2,12 +2,27 @@
 #' @param ... additional arguments passed to PrintBibliography,
 #' @return a list of works cited  
 #' @export
-bibliography <- function(...)
+bibliography <- function(..., style=NULL)
 {
   bibs <- get_bib()
-  NoCite(bibs) 
-  PrintBibliography(bibs, ...)
+
+  if(!is.null(style)){
+    refs <- sapply(bibs, csl_formatting)
+    # Provide better styling than this.  
+    cat(refs)
+  } else {
+    NoCite(bibs) 
+    PrintBibliography(bibs, ...)
+  }
   invisible(bibs)
+
+
 }
 
+
+csl_formatting <- function(bib, style = "ecology"){
+    resp <- GET(paste("http://data.crossref.org", bib$doi, sep="/"), 
+        add_headers(Accept = paste0("text/bibliography; style=", style)))
+    content(resp)
+}
 
