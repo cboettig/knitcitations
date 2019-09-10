f498ff876cb050eafe642f8c6066c13d4161ae88
Carl Boettiger <cboettig@gmail.com>
2019-09-10 12:10:19 -0700

    patch for PCRE2 compatibility

--- R/url_regexpr.R
+++ R/url_regexpr.R
@@ -1,7 +1,7 @@
 
 # http://stackoverflow.com/questions/833469/regular-expression-for-url
 
-url_regexp <- "((([A-Za-z]{3,9}:(?:\\/\\/)?)(?:[-;:&=\\+\\$,\\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\\+\\$,\\w]+@)[A-Za-z0-9.-]+)((?:\\/[\\+~%\\/.\\w-_]*)?\\??(?:[-\\+=&;%@.\\w_]*)#?(?:[.\\!\\/\\w]*))?)"
+url_regexp <- "((([A-Za-z]{3,9}:(?:\\/\\/)?)(?:[-;:&=\\+\\$,\\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\\+\\$,\\w]+@)[A-Za-z0-9.-]+)((?:\\/[\\+~%\\/.\\w\\-_]*)?\\??(?:[-\\+=&;%@.\\w_]*)#?(?:[.\\!\\/\\w]*))?)"
 
 ## Or from http://stackoverflow.com/questions/161738/what-is-the-best-regular-expression-to-check-if-a-string-is-a-valid-url
 
