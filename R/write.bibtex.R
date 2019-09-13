ea5d20259471af58cb2f68460014b3d4264da496
Carl Boettiger <cboettig@gmail.com>
2019-09-13 16:13:30 -0700

    use tempdir in example

--- R/write.bibtex.R
+++ R/write.bibtex.R
@@ -1,35 +1,37 @@
-#' write a bibtex file  
+#' write a bibtex file
 #'
-#' @param entry a 'bibentry' object or list of bibentry objects. 
-#'        If NULL, writes all that have currently been cited. 
+#' @param entry a 'bibentry' object or list of bibentry objects.
+#'        If NULL, writes all that have currently been cited.
 #' @param file output bibtex file. Will automatically append '.bib' if not
-#'  added. if 'NULL' will use stdout.  
+#'  added. if 'NULL' will use stdout.
 #' @param append a logical indicating that bibtex entries be added the the
-#'  file.  If FALSE (default), the file is overwritten.  
+#'  file.  If FALSE (default), the file is overwritten.
 #' @param ... additional arguments to WriteBib
-#' @return a list of citation information, invisibly 
-#' @import RefManageR 
+#' @return a list of citation information, invisibly
+#' @import RefManageR
 #' @examples
-#'  write.bibtex(c(citation("knitr"), 
-#'                 citation("knitcitations"), 
-#'                 citation("httr")))
-#'  bib <- read.bibtex("knitcitations.bib")
+#'  tmp <- tempfile(fileext=".bib")
+#'  write.bibtex(c(citation("knitr"),
+#'                 citation("knitcitations"),
+#'                 citation("httr")),
+#'                file = tmp)
+#'  bib <- read.bibtex(tmp)
 #' @export
 #' @seealso read.bib citep citet
-write.bibtex <- function(entry = NULL, 
-                         file = "knitcitations.bib", 
-                         append = FALSE, 
+write.bibtex <- function(entry = NULL,
+                         file = "knitcitations.bib",
+                         append = FALSE,
                          ...){
 
   if(is.null(entry)){
-    entry <- get_bib() 
+    entry <- get_bib()
 
   } else if(is(entry, "bibentry")){
     entry <- as.BibEntry(entry)
 
   } else {
     stop(paste("entry object of class", class(entry), "not recognized"))
-  
+
   }
 
   keys <- vapply(
@@ -57,5 +59,5 @@ write.bibtex <- function(entry = NULL,
 #'        BibOptions.
 #' @import RefManageR
 #' @export
-read.bibtex <- function(file, .Encoding = "UTF-8", check=FALSE) 
+read.bibtex <- function(file, .Encoding = "UTF-8", check=FALSE)
   ReadBib(file, .Encoding=.Encoding, header=NULL, footer = NULL, check=check)
--- cran-comments.md
+++ cran-comments.md
@@ -1,6 +1,6 @@
 Dear CRAN Maintainers,
 
-Minor bugfix to migrate to PCRE2 regex support.  I have also removed the spuriously included .bib file in the original submission of this release yesterday.  That release also indicated a 502 URL resolution error at the Open Source Initiative's website.  Please note that this was a chance occurance, and the the http://opensource.org is indeed a valid and well known website address, so I have made no change to this URL.
+Minor bugfix to migrate to PCRE2 regex support.  This also patches the example in write.bibtex to write to tempdir.  
 
 
 Sincerely,
